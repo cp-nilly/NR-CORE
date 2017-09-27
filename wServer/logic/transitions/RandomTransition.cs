d680604629a4c6819dca59ee0f20d4610bc7bac0
cp-nilly <cp.nilly@gmail.com>
2017-09-27 15:11:55 -0700

    Added Tuvior's RandomTransition behavior

---
+++ wServer/logic/transitions/RandomTransition.cs
@@ -0,0 +1,27 @@
+﻿using wServer.realm;
+
+namespace wServer.logic.transitions
+{
+    class RandomTransition : Transition
+    {
+        private double _probability;
+        private bool _activated;
+
+        public RandomTransition(double probability, params string[] states)
+            : base(states)
+        {
+            _probability = probability;
+            _activated = false;
+        }
+
+        protected override bool TickCore(Entity host, RealmTime time, ref object state)
+        {
+            if (_activated) return false;
+
+            _activated = true;
+
+            SelectedState = 0;
+            return Random.NextDouble() < _probability;
+        }
+    }
+}
